4a397218cdb562ecc843a960d37afc0ed3bcc260
mohammedammar735 <147385330+mohammedammar735@users.noreply.github.com>
2025-05-13 19:49:34 +0300

    Update amboss_signup.py

--- amboss_signup.py
+++ amboss_signup.py
@@ -19,9 +19,15 @@ password = ''.join(random.choices(string.ascii_letters + string.digits + "!@#$%^
 
 # Chrome options
 options = Options()
-options.add_argument("--headless")
+options = Options()
+options.add_argument("--headless=new")  # new headless mode (recommended)
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-software-rasterizer")
+options.add_argument("--remote-debugging-port=9222")
+
 
 driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options)
 wait = WebDriverWait(driver, 20)
